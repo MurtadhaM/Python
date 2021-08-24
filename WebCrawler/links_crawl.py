20006095ef54af1abffd19d8cbd5217ae730487b
Cyber Sheep <45076915+MurtadhaM@users.noreply.github.com>
2021-08-24 02:31:29 -0400

    url 2 nethods

--- WebCrawler/links_crawl.py
+++ WebCrawler/links_crawl.py
@@ -8,7 +8,20 @@ url = "https://uncc.edu"
 request = requests.get(url)
 
 # filter using regex to get links
-result = re.findall('<a .*? href="(.*?)" .*?>', request.text)
+result = re.findall('.*? href="(.*?)" .*?>', request.text)
+# Second Method
+soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+
+# Second Method
+soup.prettify()
+# Get the link
+url_list = soup.find_all('a')
+#
+# # print to terminal
+# for url in url_list:
+#     print(url.get('href'))
+
 # print to terminal
 for line_print in result:
     print(line_print)
