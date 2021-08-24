ecff583fe0e3d7977acb8a1aa2fceb1695460913
Cyber Sheep <45076915+MurtadhaM@users.noreply.github.com>
2021-08-24 09:58:09 -0400

    added main and parser in it

---
+++ Basics/main.py
@@ -0,0 +1,26 @@
+# Author: Murtadha Marzouq
+# Program to parse user input
+
+import argparse
+
+# Set a parser
+parser = argparse.ArgumentParser(description='Process Passed-in Parameters.')
+# Add a url Parameter
+parser.add_argument('--url', type=str, nargs='+', default="https://google.com",
+                    help='a string for the url (default: https://google.com)')
+# Add a depth parameter
+parser.add_argument('--depth', type=int, default='1',
+                    help='set the depth of of the crawler (default: 1)')
+# Add log to files
+parser.add_argument('--log', type=bool, default=False,
+                    help='Log to files (default: False')
+
+
+# Parse The Arguments
+args = parser.parse_args()
+# Print the url
+print("The url is: " + str(args.url))
+# Print the crawl depth
+print("The depth is: " + str(args.depth))
+# Print the log switch
+print("logging now is : " + str(args.log))
