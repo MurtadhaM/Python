26593d27f58f41c956c7ac526839d9d065618dcf
Cyber Sheep <45076915+MurtadhaM@users.noreply.github.com>
2021-08-23 21:31:36 -0400

    Links and Contents

--- Basics/start.py
+++ Basics/start.py
@@ -3,7 +3,7 @@
 # Program: Python HTTP Retrieval
 
 # Import Library to parse HTML/SOAP
-
+from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 # import Requests library
@@ -12,5 +12,22 @@ import requests
 # get the response after you run a http get request
 response = requests.get('https://www.uncc.edu')
 soup = BeautifulSoup(response.content, 'html.parser')
+
+url = "https://uncc.edu"
+html = urlopen(url)
+html_contents = BeautifulSoup(html,features="html5lib")
+
+
+# links
+links_all = html_contents.find_all("a")
+
+
+
+# search word
+html_page_pretty = html_contents.prettify()
+print(html_page_pretty)
+
+
+#print(response)
 # print
-print(soup)
+#print(soup)
