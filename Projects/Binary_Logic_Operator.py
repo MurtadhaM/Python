53e82a56f23882672de3a56388314bbe5c9500a3
Murtadha Marzouq <mmarzou0@gmail.com>
2021-12-21 04:06:26 -0500

    😎

---
+++ Projects/Binary_Logic_Operator.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+
+variables = [1,6,21,16,4,5,7]
---
+++ Projects/SMART_Contracts/contract.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+def smart_contract():
+    """
+    This is a smart contract.
+    """
+    print('This is a smart contract.')
